68a1c9153d1b8dbfeb002d71d650306d26c4ad3e
Bianca <biancakrafecik@hotmail.com>
2022-11-23 19:41:04 -0300

    Projeto Finalizado

--- src/app/previsoes.service.ts
+++ src/app/previsoes.service.ts
@@ -10,10 +10,10 @@ export class PrevisoesService {
   //Observable que permite que os valores sejam multicast para muitos Observers.
   private previsoesSubject = new Subject();
 
-  //Chave para acessar a api weather
+  //Chave para acessar a api weather.
   private appid: string = '39f24e48312f0c4e95891ff792b29c36';
 
-  //Url para acessar o banco de dados
+  //Url para acessar o banco de dados.
   private urlBd: string = 'https://gf12ad2ec957d47-databasep2previsoes.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/tb_previsoes/'
  
   //Declaração do parametro do tipo HttpClient usando o modificador private
@@ -24,7 +24,7 @@ export class PrevisoesService {
   //Essa função faz a requisição a API Weather
   //Retornando como resposta um Objeto contendo a uma lista com a previsao do tempo atual e dos próximos 5 dias
   //Passamos como parametro a cidade que desejamos buscar a previsão do tempo
-  //Seu retorno é void
+  //Seu retorno é void.
   obterPrevisoes(cidade: string): void {
     //Solicitamos uma requisição do tipo Get para buscas os dados da api
     //Na url passamos a cidade que desejamos, a chave de acesso e a unidade de medida(por padrão retorna em ºF ou ºK)
@@ -51,7 +51,7 @@ export class PrevisoesService {
   enviarPrevisao(cidade:string, data:string, link:string){ 
     //Usando o httpClient o post, que vai enviar a previsão para o nosso banco de dados
     //O post recebe como parametro a url do banco de dados (urlBd) e um objeto contendo os parametros passado
-    //Nosso subscribe apenas exibe o objeto enviado para o banco e exibe uma mensagem avisando que foi enviado
+    //Nosso subscribe apenas exibe o objeto enviado para o banco e exibe uma mensagem avisando que foi enviado.
     this.httpClient.post(this.urlBd, {cidade:cidade, data_previsao:data, link_previsao:link})
     .subscribe(resp =>{
       console.log("Enviado")
@@ -59,7 +59,7 @@ export class PrevisoesService {
     } )
   }
 
-  //Usamos o get para trazer a lista de historico das nossas requisições do nosso banco de dados e retornando-os
+  //Usamos o get para trazer a lista de historico das nossas requisições do nosso banco de dados e retornando-os.
   historicoPrevisoes(){
     return this.httpClient.get(this.urlBd)
   }
